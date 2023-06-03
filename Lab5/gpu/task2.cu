9dddd8804c0a269509e2bd8bb771ee55202150b6
Songxiao Guo <logname@mail.ustc.edu.cn>
2023-06-03 08:50:20 +0800

    finish task5.

--- Lab5/gpu/task2.cu
+++ Lab5/gpu/task2.cu
@@ -1,29 +1,57 @@
-﻿#include <cstdio>
+#include <cstdio>
 #include <cstdlib>
 #include <ctime>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 #include <random>
 #include <iostream>
+#include <chrono>
 
 #define N (1 << 11)
-#define M (N>>0)
+#define BLOCK_SIZE (1<<0)
 
-__global__ void gemm_baseline(const float* A,const float* B, float* C) {
-    int threadId = (blockIdx.y * blockDim.y + threadIdx.y) * gridDim.x * blockDim.x + blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void gemm_block(const float* A, const float* B, float* C) {
+    // thread location
+    int block_x = blockIdx.x;
+    int block_y = blockIdx.y;
+    int thread_x = threadIdx.x;
+    int thread_y = threadIdx.y;
 
-    int row = threadId / N;
-    int col = threadId % N;
+    // blockDim.x == blockDim.y == BLOCK_SIZE here
+    const int begin_a = block_y * blockDim.y * N;
+    const int end_a = begin_a + N - 1;
+    const int step_a = blockDim.x;
 
-    float t = 0.0;
-    for (size_t i = 0; i < N; ++i)
+    const int begin_b = block_x * blockDim.x;
+    const int step_b = blockDim.y * N;
+
+    float result_temp = 0.0f;
+
+    for (int index_a = begin_a, index_b = begin_b; index_a < end_a; index_a += step_a, index_b += step_b)
     {
-        t += A[row * N + i] * B[i * N + col];
+        // shared memory
+        __shared__ float SubMat_A[BLOCK_SIZE][BLOCK_SIZE];
+        __shared__ float SubMat_B[BLOCK_SIZE][BLOCK_SIZE];
+
+        // copy data to shared memory
+        SubMat_A[thread_y][thread_x] = A[index_a + thread_y * N + thread_x];
+        SubMat_B[thread_y][thread_x] = B[index_b + thread_y * N + thread_x];
+
+        __syncthreads();
+
+        for (int i = 0; i < BLOCK_SIZE; ++i)
+        {
+            result_temp += SubMat_A[thread_y][i] * SubMat_B[i][thread_x];
+        }
+
+        __syncthreads();
     }
-    C[threadId] = t;
+
+    int begin_result = block_y * blockDim.y * N + begin_b;
+    C[begin_result + thread_y * N + thread_x] = result_temp;
 }
 
-void gemm_verify(const float* A, const float* B,const float* C) {
+void gemm_verify(const float* A, const float* B, const float* C) {
     auto C_baseline = (float*)malloc(N * N * sizeof(float));
     memset(C_baseline, 0, N * N * sizeof(float));
     for (int i = 0; i < N; i++) {
@@ -39,7 +67,7 @@ void gemm_verify(const float* A, const float* B,const float* C) {
             exit(-1);
         }
     }
-    free (C_baseline);
+    free(C_baseline);
 }
 
 int main()
@@ -67,11 +95,11 @@ int main()
     cudaMemcpy(cuda_a, A, sizeof(float) * N * N, cudaMemcpyHostToDevice);
     cudaMemcpy(cuda_b, B, sizeof(float) * N * N, cudaMemcpyHostToDevice);
     // define gridsize and blocksize
-    dim3 grid(M, M, 1);
-    dim3 block(1, 1, 1);
+    dim3 grid(N/BLOCK_SIZE ,N/BLOCK_SIZE, 1);
+    dim3 block(BLOCK_SIZE, BLOCK_SIZE,1);
     // compute
     cudaEventRecord(start, 0);
-    gemm_baseline << <grid, block >> > (cuda_a, cuda_b, cuda_c);
+    gemm_block << <grid, block >> > (cuda_a, cuda_b, cuda_c);
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
     cudaMemcpy(C, cuda_c, sizeof(float) * N * N, cudaMemcpyDeviceToHost);
